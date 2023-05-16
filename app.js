4af922d3e576b938de9909848c06ab7a5074fea9
Axel Buob <buobaxel@gmail.com>
2023-05-16 10:45:58 +0200

    Conditional rendering

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
@@ -8,11 +9,14 @@ const app = Vue.createApp({
     },
     methods: {
         changeTitle(title) {
-            if(!title) {
+            if (!title) {
                 this.title = 'There is no Title'
             } else {
                 this.title = title
             }
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
         }
     }
 })
