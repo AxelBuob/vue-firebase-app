c1658942915c79d5450a52a49f0d66fbcd313531
Axel Buob <buobaxel@gmail.com>
2023-05-21 11:45:06 +0200

    13-attribute-binding

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: "https://thenetninja.co.uk",
             showBooks: true,
             books: [
                 {
                     title: 'The Final Empire',
                     author: 'Brandon Sanderson',
-                    age: 45
+                    age: 45,
+                    img: "https://picsum.photos/200/"
                 },
                 {
                     title: 'Name of the Wind',
                     author: 'Patrick Rothfuss',
-                    age: 56
+                    age: 56,
+                    img: "https://picsum.photos/200/"
                 },
                 {
                     title: 'The Way of the Kings',
                     author: 'Brandon Sanderson',
-                    age: 45
+                    age: 45,
+                    img: "https://picsum.photos/200/"
                 },
 
             ],
