820016016d0d143f5d8a578183620a175dcccc5a
Axel Buob <buobaxel@gmail.com>
2023-05-22 16:50:28 +0200

    block and results components added

--- 4-reaction-timer/src/App.vue
+++ 4-reaction-timer/src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <h1>Ninja reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <TimerBlock v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-
+import TimerBlock from './components/TimerBlock.vue'
 export default {
   name: 'App',
-  components: {
-    
+  components: { TimerBlock },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    }
   }
 }
 </script>
