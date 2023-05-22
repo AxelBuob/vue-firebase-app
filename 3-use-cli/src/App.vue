6bbb10081c790e2696a81141d5aad3881b96560a
Axel Buob <buobaxel@gmail.com>
2023-05-22 14:15:13 +0200

    challenge second modal

--- 3-use-cli/src/App.vue
+++ 3-use-cli/src/App.vue
@@ -4,14 +4,21 @@
   <div v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
       </template>
       <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
   </div>
+  <div v-if="showModalTwo">
+      <Modal @close="toggleModalTwo">
+        <h1>Sign up for the Newletter</h1>
+        <p>For updates and promo codes!</p>
+      </Modal>
+    </div>
   <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal two</button>
 </template>
 
 <script>
@@ -26,15 +33,18 @@ export default {
   data() {
     return {
       title: 'My First Vue App',
-      showModal: false
+      showModal: false,
+      showModalTwo: false
     }
   },
   methods: {
     toggleModal()
     {
       this.showModal = !this.showModal;
-    }
-
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   }
 }
 </script>
