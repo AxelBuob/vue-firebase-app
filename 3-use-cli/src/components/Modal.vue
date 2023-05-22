2093ec987e6b42c62aa8bb02cce917fc93eb0137
Axel Buob <buobaxel@gmail.com>
2023-05-22 10:47:13 +0200

    styles

--- 3-use-cli/src/components/Modal.vue
+++ 3-use-cli/src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="backdrop">
         <div class="modal">
-            <h2>Lorem ipsum dolor sit amet.</h2>
+            <h1>Lorem ipsum dolor sit amet.</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum a dolores placeat reprehenderit nihil culpa illum, porro odit quis, omnis unde non, deserunt libero repudiandae veritatis maxime! Quia, labore similique?</p>
         </div>
     </div>
@@ -14,18 +14,26 @@
 </script>
 
 <style scoped>
-   .modal {
+.modal {
     width: 400px;
     padding: 20px;
     margin: 100px auto;
     background: white;
     border-radius: 10px;
-  }
-  .backdrop {
+}
+.backdrop {
     top: 0;
     position: fixed;
     background: rgba(0,0,0,0.5);
     width: 100%;
     height: 100%;
-  }
+}
+.modal h1 {
+    color: rgb(44, 210, 210);
+    border: none;
+    padding: 0;
+}
+.modal p {
+    font-style: normal;
+}
 </style>
